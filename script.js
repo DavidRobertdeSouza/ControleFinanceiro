2cfd627cdb93d880a448b270666443f46e9addb6
David Robert de Souza <david_robert97@outlook.com>
2022-08-30 21:20:23 -0300

    Adicionando no localStorage

--- script.js
+++ script.js
@@ -4,15 +4,45 @@ const $inpQtd           = qs('#inpQtd')
 const $inpValor         = qs('#inpValor')
 const $btnCadastrar     = qs('#btnCadastrar')
 
-let dados = []
+const $form = qs('#novoItem')
 
-$btnCadastrar.onclick = cadastrarDados
+let itemLocal = JSON.parse(localStorage.getItem('item')) || []
 
-function cadastrarDados(){
-    dados.push($inpAcao.value)
-    dados.push($inpDataCompra.value)
-    dados.push($inpQtd.value)
-    dados.push($inpValor.value)
-    console.log(dados)
-    dados = []
+$form.onsubmit = (e) => {
+    e.preventDefault()
+
+    const acao = e.target.elements['inpAcao']
+    const dataCompra = e.target.elements['inpDataCompra']
+    const quantidade = e.target.elements['inpQtd']
+    const valor = e.target.elements['inpValor']
+
+    const acaoAtual = {
+        'acao': acao.value,
+        'dataCompra': dataCompra.value,
+        'quantidade': quantidade.value,
+        'valor': valor.value
+    }
+
+    localStorage.setItem('item', JSON.stringify(acaoAtual))
+
+    acao.value = ''
+    dataCompra.value = ''
+    quantidade.value = ''
+    valor.value = ''
+
+    console.log(acaoAtual)
+
+    console.log(e)
+    console.log('sa')
 }
+
+// $btnCadastrar.onclick = cadastrarDados
+
+// function cadastrarDados(){
+//     dados.push($inpAcao.value)
+//     dados.push($inpDataCompra.value)
+//     dados.push($inpQtd.value)
+//     dados.push($inpValor.value)
+//     console.log(dados)
+//     dados = []
+// }
